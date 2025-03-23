88c42c7cf8ffc8a789c31c075ec95df50f4140a2
jbittu <jaiswalbittu0319@gmail.com>
2025-03-23 17:59:52 +0530

    patch tested

--- src/index.js
+++ src/index.js
@@ -5,13 +5,42 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.json());
 app.post("/api/mentor", async (req, res) => {
   try {
     const mentor = new Mentor(req.body);
     const craetdata = await mentor.save();
     res.status(201).send(craetdata);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.get("/api/mentor", async (req, res) => {
+  try {
+    const mentorData = await Mentor.find();
+    res.status(200).send(mentorData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+app.get("/api/mentor/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const mentorData = await Mentor.find({ _id });
+    res.status(200).send(mentorData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.patch("/api/mentor/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateData = await Mentor.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    res.status(200).send(updateData);
   } catch (err) {
     res.status(400).send(err);
   }
